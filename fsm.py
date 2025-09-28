97668728c65b2b75430ea8597fef5f00847188b4
MARVRK <bfg578@gmail.com>
2025-09-28 16:42:19 +0200

    implement logic to FSM

--- fsm.py
+++ fsm.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass
 from enum import auto, Enum
+import random
 
 
 @dataclass
 class PlayerData:
     player_name: str
+
     scores: int = 0
     level: int = 0
+    selected_word: str = ""
+    hint: str = ""
+    tries_left: int = 0
 
 
 class GameFSM(Enum):
@@ -21,10 +26,38 @@ class GameFSM(Enum):
 class GameManager:
     player: PlayerData
     state: GameFSM
+    counter = 0
+    string_storage = ""
+    amount_to_guess = {"easy": 5, "medium": 3, "hard": 2}
+    words_to_guess = {"easy": {"blazing": "rust", "superman": "famous comics hero"},
+                      "medium": {"digital": "signal is analog or ....", "iqos": "smoke"},
+                      "hard": {"iphone": "apple", "python": "so slow"}}
+    menus_tab = "1.Start_Game \n2.Start Random Game with Random Difficulty \n3.End_Game"
 
     def start_game(self):
+
         if self.state == GameFSM.IDLE:
-            print("Game in awaiting state")
+            print("Launching game in playing state")
+            print("Welcome to the hangman game!\ntype :help")
+
+            difficulty = random.choice(list(self.amount_to_guess.keys()))
+            level = self.words_to_guess.get(difficulty)
+
+            self.player.tries_left = self.amount_to_guess[difficulty]
+            self.player.selected_word = random.choice(list(level.keys()))
+            self.player.hint = level[self.player.selected_word]
+
+            self.state = GameFSM.PLAYING
+
+    def guess_word(self, word: str):
+        if self.state == GameFSM.PLAYING:
+            if word in self.player.selected_word:
+                print(f"Correct letter: {word}")
+                print(f"Amount of guess words left: {self.player.tries_left - self.counter}")
+            else:
+                self.counter +=1
+                print(f"Incorrect letter: {word}")
+                print(f"Wrong, Amount of guess words left: {self.player.tries_left - self.counter}")
 
     def play(self):
         if self.state == GameFSM.PLAYING:
@@ -48,10 +81,12 @@ class GameManager:
         return self.player.scores, self.player.level, self.player.player_name
 
 
-state = GameManager(PlayerData(player_name="John"), state=GameFSM.WON)
-state.play()
-state.play()
-state.play()
-state.play()
-print(state.player.scores)
-print(state.results())
+state = GameManager(PlayerData(player_name="John"), state=GameFSM.IDLE)
+state.start_game()
+state.guess_word("blazing")
+# state.play()
+# state.play()
+# state.play()
+# state.play()
+# print(state.player.scores)
+# print(state.results())
