fd87b42fec85cff8a281b62f32f56634eaf6d473
Rostyslav <rkondratiuk@solutions4ga.com>
2025-10-24 13:18:59 +0200

    finishing repo.py for player and game

--- repo.py
+++ repo.py
@@ -1,47 +1,56 @@
+import uuid
 from abc import ABC, abstractmethod
 from fsm import GameManager
+from repoimpl import DataBase
+
+
+db = DataBase()
 
 class FSMAbstraction(ABC):
     @abstractmethod
-    def get_fsm(self, fsm_id):
+    def get_fsm(self, fsm_id: uuid.UUID):
         pass
 
     @abstractmethod
-    def save_fsm(self, entity: GameManager):
+    def save_fsm(self, game: GameManager):
         pass
 
 
 class PlayerAbstraction(ABC):
     @abstractmethod
-    def get_player(self, player_id):
+    def get_player(self, player_id: GameManager):
         pass
 
     @abstractmethod
-    def save_player(self, player: GameManager):
+    def save_player(self, player_name: str ):
         pass
 
 
 class GameRepository(FSMAbstraction):
-    def get_fsm(self, fsm_id):
-        # from repoimpl will be function to gain fsm_id
-        request = db.filter(fsm_id)
+    def get_fsm(self, fsm_id: uuid.UUID):
+        request = db.get_game(game_id=fsm_id)
         if request:
             return {"game found": f"{request}"}
         return {"message": "no game found"}
 
-    def save_fsm(self, fsm_entity):
-        #from repoimpl will be function to save fsm_entity
-        return {"message": f"{fsm_entity} successfully saved"}
+    def save_fsm(self, fsm_entity: GameManager):
+        try:
+            db.store_game(data=fsm_entity)
+            return {"message": f"{fsm_entity} successfully saved"}
+        except Exception as e:
+            raise e
 
 
 class PlayerRepository(PlayerAbstraction):
-    def get_player(self, player_id):
-        # from repoimpl will be function to gain fsm_id
-        request = db.filter(player_id)
+    def get_player(self, player_id: int):
+        request = db.get_name(player_id)
         if request:
             return {"player found": f"{request}"}
         return {"message": "no player found"}
 
-    def save_player(self, player_entity):
-        # from repoimpl will be function to save fsm_entity
-        return {"message": f"{player_entity} successfully saved"}
+    def save_player(self, player_name):
+        try:
+            request = db.store_name(player_name)
+            return {f"player saved successfully with ID: {request}"}
+        except Exception as e:
+            raise e
